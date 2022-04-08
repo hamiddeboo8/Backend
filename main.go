32bd6541f7106f1cb4b0e3b69267e353aeda5e3a
hamiddeboo8 <hamiddeh19@gmail.com>
2022-04-08 14:08:29 +0430

    checking github

--- main.go
+++ main.go
@@ -10,30 +10,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CORS(c *gin.Context) {
-
-	// First, we add the headers with need to enable CORS
-	// Make sure to adjust these headers to your needs
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "*")
-	c.Header("Access-Control-Allow-Headers", "*")
-	c.Header("Content-Type", "application/json")
-
-	// Second, we handle the OPTIONS problem
-	if c.Request.Method != "OPTIONS" {
-
-		c.Next()
-
-	} else {
-		fmt.Println(c.Request.Body)
-		// Everytime we receive an OPTIONS request,
-		// we just return an HTTP 200 Status Code
-		// Like this, Angular can now do the real
-		// request using any other method than OPTIONS
-		c.AbortWithStatus(200)
-	}
-}
-
 var (
 	docService    service.DocService       = service.New()
 	DocController controller.DocController = controller.New(docService)
